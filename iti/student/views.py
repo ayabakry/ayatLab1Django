32d3c2c8e5637fdbc7c620320b3afb4f2b38d43a
ayatkhaled <ayatbakry@yahoo.com>
2022-06-28 13:38:52 +0200

    lab1

---
+++ iti/student/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import  HttpResponse,HttpResponseRedirect
+
+# Create your views here.
+def studentlist(request):
+    return HttpResponse('List student')
+def studentinsert(request):
+    return render (request,'insert.html')
+def studentupdate(request):
+    return render (request,'update.html')
+def index(request):
+    return render (request,'index.html')
+def studentdelete(request):
+    return HttpResponseRedirect('/list')
